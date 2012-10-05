a2fa6069af27e538779fb8d9428e49fd3e37bd37
Johan Redestig <johan.redestig@sonymobile.com>
2012-10-05 10:34:03 +0200

    gps: Add gps feature
    Change-Id: I68d3cb0de737787a2ea0f4e6a132c0444b686226

--- device.mk
+++ device.mk
@@ -37,7 +37,8 @@ $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
 
 # This device is xhdpi.  However the platform doesn't
 # currently contain all of the bitmaps at xhdpi density so

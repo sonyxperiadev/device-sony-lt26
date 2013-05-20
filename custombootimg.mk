16eaf53f913c858970a0797e0d4d679ed02d9073
Johan Redestig <johan.redestig@sonymobile.com>
2013-05-20 12:49:10 +0200

    Do custom boot.img without BOARD_CUSTOM_BOOTIMG_MK
    This is useful until the custom boot img patch is merged
    upstream.
    
    Change-Id: Ib1a20cbb4a48d04d90394ae338c9ba3829e000db

--- custombootimg.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-MKELF := device/sony/lt26/tools/mkelf.py
-INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(INSTALLED_RAMDISK_TARGET) $(MKBOOTIMG) $(MINIGZIP) $(INTERNAL_BOOTIMAGE_FILES)
-	$(call pretty,"Boot image: $@")
-	$(hide) python $(MKELF) -o $@ $(PRODUCT_OUT)/kernel@0x40208000 $(PRODUCT_OUT)/ramdisk.img@0x41300000,ramdisk vendor/sony/lt26/proprietary/boot/RPM.bin@0x20000,rpm
-
